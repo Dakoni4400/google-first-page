06cf9af1c33003ffc9143abbf993bc14077a033e
Till Kobbe <kobbe@lean-ocean.com>
2022-08-31 15:07:43 +0200

    add service for first page

---
+++ src/services/GoogleFirstPageService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RandomUser\GoogleFirstPage\services;
+
+class GoogleFirstPageService
+{
+    public function firstPage()
+    {
+        throw new \Exception("This package is broken");
+    }
+}
